bb5fdfcf04eb7e97d9b8046a5fbb291ad20feaef
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-07-28 10:05:11 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-'use strict';
- 
-// Application Dependencies
-const express = require('express');
-require('dotenv').config();
- 
-// Application Setup 
-const app = express();
-const PORT = process.env.PORT || 3000;
- 
-// Route Definitions
- 
-// Route Handlers
- 
-// App listener
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
-DeltaV Code School Logo
-PORT=3001
-server.js
-We don't have a homepage yet so the browser displays "Cannot GET /". Let's run a .get() function on our app to respond with a simple greeting. (Lines 12, 16-18)
-
-This code says when someone requests the "/greet" path of our website we want to execute a callback function that responds with a 200 (OK) status code and send them a simple message.
-
-If you add app.use('*', ...), any other requests will be served a 404 (Not found) message. (Line 13, 21-22)
 
 'use strict';
  
@@ -34,16 +10,40 @@ const app = express();
 const PORT = process.env.PORT || 3000;
  
 // Route Definitions
+app.use(express.static('./public'));
 app.get('/greet', greetHandler);
+app.get('/data', dataHandler);
+app.get('/bad', (request, response) => { throw new Error('Sorry the the inconvenience.'); });
 app.use('*', notFoundHandler);
+app.use(errorHandler);
  
 // Route Handlers
 function greetHandler(request, response) {
   response.status(200).send('Hello World!');
 }
  
+function dataHandler(request, response) {
+  const airplanes = [
+    {
+      departure: new Date(2020, 7, 24),
+      canFly: true,
+      pilot: 'Well Trained'
+    },
+    {
+      departure: new Date(2020, 7, 25),
+      canFly: false,
+      pilot: 'NA'
+    }
+  ];
+  response.status(200).json(airplanes);
+}
+ 
 function notFoundHandler(request, response) {
   response.status(404).json({ notFound: true });
+}
+ 
+function errorHandler(error, request, response, next) {
+  response.status(500).json({ error: true, message: error.message });
 }
  
 // App listener
